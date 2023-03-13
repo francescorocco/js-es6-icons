2854ae670b5ee5f87721c1a702bf0f446eda63d3
Francesco Rocco <francescorocco97@gmail.com>
2023-03-13 15:42:08 +0100

    Milestone 3 completato

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
 //Milestone 2
 //Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+//Milestone 3
+//Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
 
 const listElements = [
 	{
@@ -118,12 +120,33 @@ const listElements = [
 	}
 ];
 const containerDom = document.getElementById('container');
+let selectDom = document.getElementById('selector');
+let choosenSelected = selectDom.value;
+
 
 listElements.forEach(element =>{
 	let newCard = createCard(element.name, element.prefix, element.color);
 	containerDom.append(newCard);
 });
 
+
+selectDom.addEventListener('change',function(){
+	let choosenSelected = selectDom.value;
+	const filteredElements = listElements.filter(element =>{
+		if( element.type == choosenSelected){
+			return element;
+		}else if( choosenSelected == ''){
+			return element;
+		}
+	});
+
+	containerDom.innerHTML = '';
+
+	filteredElements.forEach(element =>{
+		let newCard = createCard(element.name, element.prefix, element.color);
+		containerDom.append(newCard);
+	});
+});
 
 
 function createCard(name, prefix, color){
@@ -141,3 +164,8 @@ function createCard(name, prefix, color){
 
 	return cardbox
 }
+
+// function appendCard(){
+// 	let newCard = createCard(element.name, element.prefix, element.color);
+// 		containerDom.append(newCard);
+// }
